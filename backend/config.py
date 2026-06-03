449d13f6cefbf32f40718769dbcffb6a2ec68d0b
Yasaswini <214g1a05c2@srit.ac.in>
2026-06-03 18:36:38 +0530

    Fix database URL

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://ai_admin:xxxxx@dpg-xxxxx-a.singapore-postgres.render.com/ai_voice_agent_6bb7"
+    SQLALCHEMY_DATABASE_URI = "postgresql://ai_admin:bWZYiVndbkimE6oTJ7HNw7GligOWHAE2@dpg-d8g0hhuk1jcs73d2jts0-a/ai_voice_agent_6bb7"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
