79c3bda0c22ff15414599f365d44928eee8dc0b3
Yasaswini <214g1a05c2@srit.ac.in>
2026-06-08 17:51:42 +0530

    Updated font to Poppins

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,7 @@
-import os
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://ai_admin:bWZYiVndbkimE6oTJ7HNw7GligOWHAE2@dpg-d8g0hhuk1jcs73d2jts0-a/ai_voice_agent_6bb7"
+
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql://postgres:binny905@localhost:5433/postgres"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
